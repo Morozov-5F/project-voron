fec6741c51945397e2bcfd1f38fc4386cb57bb04
Morozov-5F <morozov.evgeniy.95@gmail.com>
2021-01-07 15:02:09 +0300

    Add basic loader and linker script.

---
+++ loader.s
@@ -0,0 +1,21 @@
+; Copyright (c) 2020, Evgeniy Morozov
+; All rights reserved.
+; This source code is licensed under the BSD-style license found in the
+; LICENSE file in the root directory of this source tree.
+
+global loader
+
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS        equ 0x0
+CHECKSUM     equ -MAGIC_NUMBER
+
+section .text:
+align 4
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
+
+.loader:
+    mov eax, 0xCAFEBABE
+.loop:
+    jmp .loop
